3a7247aaafdf6548cf2c3fbaabb2b3c24030e321
Heng <bunsanphe@gmail.com>
2020-06-29 23:48:55 -0700

    added folder structures and files

---
+++ app/db/seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Cheeseburger", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Veggieburger", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Chickenburger", true);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Elkburger", true);
---
+++ server.js
@@ -0,0 +1,3 @@
+const express = require("express")
+const connection = require("connection")
+const orm = require("./config/orm")
